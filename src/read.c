1b7e27452e816fc6d3ce61c183acc4e5b4b85d3b
Roberto Baquerizo <roba8460@colorado.edu>
2019-02-02 21:40:02 -0700

    defines and implements read function (ready entire file)

---
+++ src/read.c
@@ -0,0 +1,29 @@
+#include "read.h"
+
+void read( file_t* p_file, char* p_action, char* p_input )
+{
+   if( NULL == p_file->p_file || (0 != strcmp(p_file->name, p_input)) )
+   {
+      fprintf( stderr, "Error reading file \"%s\"\n: No such file or directory.\n",
+               p_input );
+      return;      
+   }
+   else if( 0 != strcmp("read", p_action))
+   {
+      printf( "Invalid action!" );
+      return;
+   }
+   if( READ != p_file->mode || READU != p_file->mode )
+   {
+      fprintf( stderr, "Error reading file \"%s\": Permission denied.\n",
+            p_file->name );
+      return;
+   }
+   char str[MAX_INPUT_LENGTH];
+   while( NULL != fgets( str, MAX_INPUT_LENGTH, p_file->p_file ) )
+   {
+      puts( str );
+   }
+   rewind( p_file->p_file );
+   return;
+}
