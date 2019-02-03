74def0682dc14285b2146b26452fc95cab4e4ada
Roberto Baquerizo <roba8460@colorado.edu>
2019-02-03 08:19:01 -0700

    removed debug fprintf

--- src/write.c
+++ src/write.c
@@ -16,14 +16,11 @@ void write( file_t* p_file, char* p_action, char* p_input )
    }
    if( 0 == strcmp( "write", p_action ) )
    {
-      int num = 0;
-      if( 0 > ( num = fprintf( p_file->p_file, "%s\n", p_input ) ) )
+      if( 0 > fprintf( p_file->p_file, "%s\n", p_input ) )
       {
          perror( "ERROR" );
          return;
       }
-      fprintf( stdout, "Wrote %u characters to file \"%s\".\n",
-               num, p_file->name );
    }
    else
    {
